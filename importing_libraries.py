3d0c33c257c30b6c283f06ee9d5d79c0af611b37
Deepika katreddy <131348836+deepikakatreddy@users.noreply.github.com>
2024-06-25 15:03:50 +0530

    importing_libraries.py

---
+++ importing_libraries.py
@@ -0,0 +1,13 @@
+# Importing necessary libraries
+import pandas as pd
+import numpy as np
+from matplotlib import pyplot as plt
+%matplotlib inline
+import seaborn as sns
+
+# Ignore the harmless warnings
+import warnings
+warnings.filterwarnings('ignore')
+
+# Set to display all columns in dataset
+pd.set_option('display.max_columns', None)
